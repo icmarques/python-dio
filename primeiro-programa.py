501ebb1ac1293de91ee703ebd954ecf51d6a1a2d
Carla Marques <128189025+icmarques@users.noreply.github.com>
2023-08-20 16:37:22 -0300

    Add files via upload

---
+++ primeiro-programa.py
@@ -0,0 +1,12 @@
+#texto: str
+#numerico: int, float, complex
+#sequencia: list, tuple, range
+#mapa (chave, valor): dict
+#colecao (parece com uma lista, mas sem elementos repetidos): set, frozenset
+#booleano: bool
+#binário: bytes, bytearray, memoryview
+
+print(11+10) #int
+print(1.5+2+2.7) #float e int
+print(True) #bool
+print("Python") #str

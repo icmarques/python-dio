fba155cc24de0721f36606e5135968a10782109a
Carla Marques <carlamarques.dev@gmail.com>
2023-08-21 08:43:06 -0300

    Continuação estudo de variaveis

---
+++ variaveis.py
@@ -0,0 +1,14 @@
+#continuação estudo variáveis
+
+age = 23
+name = "Guilherme"
+print(f"Meu nome é {name} e eu tenhp {age} ano(s) de idade.")
+
+
+age, name = (27, "Giovanna")
+print(f"Meu nome é {name} e eu tenhp {age} ano(s) de idade.")
+
+#uma constante nasce com um valor e permanece com ele até o final da execução do programa, ou seja, o valor é imutável.
+#em python usa-se uma convenção para se dizer que uma variável é constante: deve-se criar a variável com o nome todo em letras maiúsculas.
+
+

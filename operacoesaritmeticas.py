1cce282781553eb413ffd11fad54db16887b9a75
Carla Marques <carlamarques.dev@gmail.com>
2023-08-23 08:46:56 -0300

    Arquivo de operaçoes aritmeticas

---
+++ operacoesaritmeticas.py
@@ -0,0 +1,33 @@
+# Adição
+    #print(1+1)
+    #>>>> 2
+
+# Subtração
+    #print(10-2)
+    #>>>> 8
+
+# Multiplicação
+    #print(4*3)
+    #>>>> 12
+
+# Divisão
+    #print(12 / 3)
+    #>>>> 4.0
+    
+# Divisão inteira
+    #print(12 // 2)
+    #>>>> 6
+    
+# Módulo
+    #print(10 % 3)
+    #>>>> 1
+
+# Exponenciação
+    #print(2 ** 3)
+    #>>>> 8
+    
+#min 02:25h
+    
+
+    
+

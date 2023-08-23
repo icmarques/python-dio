4f91ec07da58eff19ad533718403601df4f296ba
Carla Marques <carlamarques.dev@gmail.com>
2023-08-23 09:31:25 -0300

    .

---
+++ operadorescomparacao.py
@@ -0,0 +1,27 @@
+#igualdade
+  #  ==
+    
+#diferença
+  #  !=
+
+#maior que
+  #  >
+
+#Maior que ou igual
+  #  >=
+  
+#menor que
+  #  <
+
+#menor que ou igual
+  #  <=
+  
+# Sempre que um valor de comparação for impresso, ele vai retornar um valor boleano
+
+    #saldo = 5
+    #novo_saldo = 10
+    #print(saldo == novo_saldo)
+    #>>>> False
+    
+    #print(saldo != novo_saldo)
+    #>>>> True

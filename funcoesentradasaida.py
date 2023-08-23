21110fce02846f54db77793b10a72d803fd01b0a
Carla Marques <carlamarques.dev@gmail.com>
2023-08-23 08:27:48 -0300

    Arquivo funcoes input e output

---
+++ funcoesentradasaida.py
@@ -0,0 +1,12 @@
+#input
+    #é uma funçào builtin (pois já é da linguagem do python)
+    #ela recebe um argumento do tipo string, que é exibido para o usuário na saída padr"ao (tela)
+    #a função lê a entrada, converte para string e retorna o valor
+    
+    
+    
+#print
+    #é uma funçào builtin (pois já é da linguagem do python)
+    #ela recebe um arguento obrigatório do tipo varargs de objetos e 4 argumentos opcionais (sep, end, file e flush)
+    #todos os objetos são convertidos para string, separados por sep e terminados por end
+    #a string final é exibida para o usuário

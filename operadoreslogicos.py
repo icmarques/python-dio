7f81162036a51e2692d01071784e134227f0eca3
Carla Marques <carlamarques.dev@gmail.com>
2023-08-24 18:02:43 -0300

    Arquivo operações logicas

--- operadoreslogicos.py
+++ operadoreslogicos.py
@@ -1,3 +1,48 @@
 #São operadores utilizados em conjunto com operadores de comparação para montar uma expressão lógica.
 #Quando um operador de comparação é utilizado, o resultado retornado é um booleano, dessa forma podemos combinar operadores de comparação com os operadores lógicos.
     #Ex: op_comparacao + op_logico + op_comparacao ... N ...
+    #saldo = 1000
+    #saque = 200
+    #limite = 100
+    
+    #saldo >= saque
+    #>>>> True
+    
+    #saque <= limite
+    #>>>> False
+    
+#Operador E
+
+ #saldo = 1000
+    #saque = 200
+    #limite = 100
+    
+    #saldo >= saque and  #saque <= limite
+    #>>>> False
+    
+    
+#Operador OU
+    
+    #saldo = 1000
+    #saque = 200
+    #limite = 100
+    
+    #saldo >= saque or  #saque <= limite
+    #>>>> True
+    
+    
+#Operador Negação
+
+#contatos_emergencia = []
+
+#not 1000 > 1500
+#>>>> True
+
+#not contatos_emergencia
+#>>>> True
+
+#not "saque 1500;"
+#>>>> False
+
+#not ""
+#>>>> True

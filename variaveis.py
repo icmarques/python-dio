ffc71a9f6e277283257e530a8d90749f432d1f1c
Carla Marques <carlamarques.dev@gmail.com>
2023-08-24 18:04:00 -0300

    variaveis

--- variaveis.py
+++ variaveis.py
@@ -2,11 +2,11 @@
 
 age = 23
 name = "Guilherme"
-print(f"Meu nome é {name} e eu tenhp {age} ano(s) de idade.")
+print(f"Meu nome é {name} e eu tenho {age} ano(s) de idade.")
 
 
 age, name = (27, "Giovanna")
-print(f"Meu nome é {name} e eu tenhp {age} ano(s) de idade.")
+print(f"Meu nome é {name} e eu tenho {age} ano(s) de idade.")
 
 #uma constante nasce com um valor e permanece com ele até o final da execução do programa, ou seja, o valor é imutável.
 #em python usa-se uma convenção para se dizer que uma variável é constante: deve-se criar a variável com o nome todo em letras maiúsculas.

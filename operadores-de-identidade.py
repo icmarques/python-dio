cf2348db7a52aaa35571f93ea8d2f63ca65b36ae
Carla Marques <carlamarques.dev@gmail.com>
2023-08-27 17:18:14 -0300

    Arquivos de indentação, associação e de identidade

---
+++ operadores-de-identidade.py
@@ -0,0 +1,16 @@
+#Säo opradores utilizados para comparar se os dois objetos testados ocupam a mesma posição na memória.
+
+#curso = "Curso de python"
+#nome_curso = curso
+#saldo, limite = 200, 200
+
+#curso is nome_curso
+#>>>> True
+
+#curso is not onome_curso
+#>>>> False
+
+#saldo is imite
+#>>>> True
+
+

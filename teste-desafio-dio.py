834b96b566e48f4c2ab6f7754a8d1a36a48f74c2
Carla Marques <carlamarques.dev@gmail.com>
2023-09-01 18:03:14 -0300

    .

---
+++ teste-desafio-dio.py
@@ -0,0 +1,27 @@
+import sys
+#a = int(sys.stdin.readline())
+#print(a)
+
+# T = int(input("Tweet:"))
+
+# if T <= int(140):
+#     verificacao = (str("TWEET"))
+#     verificacao_2 = 0
+#     print(verificacao)
+  
+# else:
+#     verificacao_2 = (str("MUTE"))
+#     verificacao = 0
+#     print(verificacao_2)
+
+T = input()
+print(T)
+
+len(T)
+
+if len(T) <= (140):
+    print("TWEET")
+    
+else:
+    print("MUTE")
+

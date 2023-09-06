076ea8e9a82b359cc7a0d0cc3eb68ebcbf29ed7e
Carla Marques <carlamarques.dev@gmail.com>
2023-09-06 09:43:13 -0300

    Ajuste nome desafio 1 e inicio desafio 2 Criacao do arquivo do desafio sistema bancario

--- desafio-dio-2.py
+++ desafio-dio-2.py
@@ -1,10 +1,22 @@
 import sys
 
 month = input()
-month_dict = month
+months_dict = list(mes_, mes_1, mes_2, mes_3, mes_4, mes_5, mes_6, mes_7, mes_8, mes_9, mes_10, mes_11, mes_12)
 
-mes_1 = 1
-mes_2 = 2
+mes_1 = "January"
+mes_2 = "February"
+mes_3 = "March"
+mes_4 = "April"
+mes_5 = "May"
+mes_6 = "June"
+mes_7 = "July"
+mes_8 = "August"
+mes_9 = "September"
+mes_10 = "October"
+mes_11 = "November"
+mes_12 = "December"
 
-if month_dict == 1:
-    print("January")
+if month == 1:
+    print(months_dict(range()))
+
+

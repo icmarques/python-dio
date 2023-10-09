58b19874988ffa38abe5c8011dff3f7a113fd853
Carla Marques <carlamarques.dev@gmail.com>
2023-10-09 17:29:44 -0300

    Arquivo dicionário completo e novo arquivo de metoos-dicionario

--- 2. Estrutura de dados/dicionarios.py
+++ 2. Estrutura de dados/dicionarios.py
@@ -1,5 +1,5 @@
 #É um conjunto não ordenado de pares chave: valor onde as chaves são únicas em uma dada instancia do dicionario.
-#São delimitados por: {} e contém uma lista de pares chace: valor separada por vírgulas. A atribuição é feita com :
+#São delimitados por: {} e contém uma lista de pares chave: valor separada por vírgulas. A atribuição é feita com :
 #Pode-se usar também a classe dict, onde a chave e o valor ficarão entre parenteses e atribuição é feita pelo sinal de =, dividindo as duplas por virgula
 
 pessoa = {"nome": "Guilherme", "idade": 28} #a chave é o nome e o valor é Guilherme; chave idade e valor 28
@@ -9,7 +9,7 @@ pessoa = dict(nome="Guilherme", idade=28) #a chave é o nome e o valor é Guilhe
 pessoa["telefone"] = "3333-3333" #{"nome": "Guilherme", "idade": 28, "telefone": "3333-3333"}
 
 
-#Para acessar o argumento que está dentro do dicionário, deverá:
+# Para acessar o argumento que está dentro do dicionário, deverá:
     #ser usada o nome do dicionário (a variável que identifica);
     #[];
     #e o nome da chave dentro dos conchetes
@@ -26,10 +26,12 @@ dados["telefone"] = "1111-1111"
 
 #dados = "nome": "Maria", "idade": 18, "telefone": "1111-1111"
 
+#dicionários NÂO PERMITE que repitam chaves, então ele sobrescreve valores quando indicados com a mesma chave
+
 
 
 # Dicionários aninhados
-#Dicionários podem armazenar qualquer tipo de objeto Python como valor, desde uqe a chave para esse valor seja um objeto imutável como strings e números.
+#Dicionários podem armazenar qualquer tipo de objeto Python como valor, desde que a chave para esse valor seja um objeto imutável como strings e números.
 
 contatos = {
     "guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-3333"}
@@ -41,3 +43,17 @@ contatos = {
 
 contatos["guilherme@gmail.com"]["telefone"] # "3443-2121"
 
+#Neste exemplo, é informado primeiro o nome do dicionário (contatos), depois o nome do segundo dicionário ("guilherme@gmail.com") e por fim, a info de dentro do dicionário que eu quero saber.
+
+
+
+# Iterar dicionários
+#A forma mais comum para percorrer os dados de um dicionário é utilizando um comando for.
+
+for chave in contatos: #aqui é a chave dentro do dicionário
+    print(chave, contatos[chave])
+        
+for chave, valor in contatos.items(): #aqui é a chave e o valor dentro do dicionário
+    print(chave, valor)
+    
+    

d2ffdd13dab6e9b4cdaa7dcdaecaea6b84e204b3
Carla Marques <carlamarques.dev@gmail.com>
2023-10-21 18:03:19 -0300

    Exercício finalizado

--- Desafios/Fundamentos/desafio-sistema-bancario.py
+++ Desafios/Fundamentos/desafio-sistema-bancario.py
@@ -1,7 +1,7 @@
 #O sistema deve permitir realizar 3 saques diários com limite máximo de R$500,00 por saque - OK
 #Caso o usuário não tenha saldo em conta, o sistema deve exibir uma mensagem informando que não será 
 #possível sacar o dinheiro por falta de saldo - OK
-#Todos os saques devem ser armazenados em uma variável e exibidos na operação de extrato
+#Todos os saques devem ser armazenados em uma variável e exibidos na operação de extrato ok
 
 menu = """
 Escolha uma opção abaixo: 
@@ -18,6 +18,8 @@ limite = 500
 extrato = 0
 numero_saques = 0
 limite_saques = 3
+extrato_deposito = 0
+extrato_saque = 0
 
 
 while True:
@@ -30,6 +32,7 @@ while True:
         valor_deposito = float(input())
         saldo_atual_deposito = saldo + valor_deposito
         saldo = saldo_atual_deposito
+        extrato_deposito += valor_deposito
         print(f"Seu saldo atual é: R$ {saldo}")
         
         
@@ -51,6 +54,7 @@ while True:
                 saldo = saldo_atual_saque
                 operacao_saque = limite_saques - 1
                 limite_saques = operacao_saque
+                extrato_saque += valor_saque
                 print(f"Seu saldo atual é: R$ {saldo}")
                 
         elif limite_saques <= 0:
@@ -59,8 +63,15 @@ while True:
             
     elif opcao == "e":
         print("Opção selecionada: Extrato")
-        print("Total sacado: ")
-        print(f"Total atual: R$ {saldo}")
+        
+        if extrato_deposito != 0:
+            print(f"Total depósitos: R$ {extrato_deposito:.2f}")
+            
+        elif extrato_saque != 0:
+            print(f"Total saques: R$ {extrato_saque:.2f}")
+            
+        
+        print(f"Saldo: R$ {saldo:.2f}")
        
        
     elif opcao == "q":

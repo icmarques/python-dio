c09cc0d1d320b9f6293619e9b068402bdbc17e0e
carla marques <carlamarques.dev@gmail.com>
2023-10-23 09:56:41 -0300

    Arquivo POO

---
+++ 3. Objetos orientacao/POO.py
@@ -0,0 +1,22 @@
+Paradigmas de programação
+
+#Um paradigma de programação é um estilo e programação.
+#Não é uma linguagem e sim a forma como você soluciona os problemas através do código.
+
+Exemplo:
+    #problema: Beber água
+    #Solução 1: Usar um copo para beber água
+    #Solução 2: Usar uma garrafa para beber água
+    
+Alguns paradigmas:
+    
+#Imperativo ou procedural;
+#Funcional;
+#Orientada a eventos.
+
+
+Programação orientada a objetos
+
+#O paradigma de programação orientada a objetos estrutura o código abstraindo problemas em objetos do mundo real, facilitando o entendimento do código e tornando-o mais modular e extensível.
+#Os dois conceitos chaves para aprender POO são: classes e objetos
+

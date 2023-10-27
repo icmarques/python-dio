ba0466c8cb3359a2e51ace4a6650a6376418a9d6
carla marques <carlamarques.dev@gmail.com>
2023-10-27 11:49:19 -0300

    Arquivo metodo construtor

---
+++ 3. Objetos orientacao/construtores-e-destrutores.py
@@ -0,0 +1,8 @@
+Métodos __init__ e __del__
+
+Método construtor (inicializador)
+#O método construtor sempre é executado quando uma nova instância da classe é criada.
+#Nesse método inicializamos o estado do nosso objeto.
+#Para declarar o método construtor da classe, criamos um método com o nome __init__.
+
+

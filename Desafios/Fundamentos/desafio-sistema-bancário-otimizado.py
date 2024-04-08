65c19480b6251f44459fa5ce1e6981b58205f7b1
carla marques <carlamarques.dev@gmail.com>
2024-04-08 13:22:07 -0300

    criação classe arquivo bancário

---
+++ Desafios/Fundamentos/desafio-sistema-bancário-otimizado.py
@@ -0,0 +1,15 @@
+class Conta_Bancária:
+    
+    def saldo (self,valor=0):
+        self.valor = valor
+        
+    def saque (self,valor):
+        self.valor = valor
+        
+    def deposito (self,valor):
+        self.valor = valor
+    
+    def extrato (self,extrato):
+        self.valor = valor
+        
+    
